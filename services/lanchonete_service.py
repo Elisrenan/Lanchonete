249989634ac1ef575a67f7dffd5b0797d460f384
Elisrenan <elisrenan@gmail.com>
2026-03-05 21:07:16 -0300

    Adiciona método para criar produto no serviço Lanchonete

--- services/lanchonete_service.py
+++ services/lanchonete_service.py
@@ -1,4 +1,5 @@
 from domain.cliente import Cliente
+from domain.produto import Produto
 from repositories.memory import db
 
 class LanchoneteService:
@@ -13,8 +14,10 @@ class LanchoneteService:
     def obter_cliente(self, cpf: str) -> Cliente | None:
         return db.clientes_por_cpf.get(cpf)
 
-    def criar_produto(self):
-        pass
+    def criar_produto(self, id: int, tipo: int, valor: float, desconto_percentual: float = 0.0) -> Produto:
+        produto = Produto(id=id, tipo=tipo, valor=valor, desconto_percentual=desconto_percentual)
+        db.produtos_por_id[id] = produto
+        return produto
 
     def obter_produto(self):
         pass
