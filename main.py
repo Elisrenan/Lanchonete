450c4492fbb653cdda50b5716eb27c2af059fdfa
Elisrenan <elisrenan@gmail.com>
2026-03-09 19:27:09 -0300

    Atualiza a configuração do FastAPI para incluir descrição e versão no projeto Lanchonete

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from api.routes.health import router as health_router
 from api.routes.clientes import router as clientes_router
 from api.routes.produtos import router as produto_router
 
-app = FastAPI(title="Exercício Lanchonete")
+app = FastAPI(
+                title="Projeto Lanchonete", 
+                description="API para gerenciamento de clientes e produtos da lanchonete", 
+                version="1.0.0"
+            )
 
 app.include_router(health_router)
 app.include_router(clientes_router)
